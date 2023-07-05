5342d5c93aaa069db622f476a1adf31fdfdce5c6
Andreea Meirosu <ameirosu@uw.co.uk>
2023-07-05 14:23:41 +0300

    cors changes

--- cmd/opt-out/internal/api/server.go
+++ cmd/opt-out/internal/api/server.go
@@ -84,12 +84,11 @@ func (s *OptOutServer) Register(router *mux.Router) {
 // EnableCORS enables adding CORS headers.
 func EnableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if origin := r.Header.Get("Origin"); origin != "" {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-			w.Header().Set("Access-Control-Allow-Headers",
-				"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		}
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
 		// Stop here if its Preflighted OPTIONS request
 		if r.Method == "OPTIONS" {
 			return
