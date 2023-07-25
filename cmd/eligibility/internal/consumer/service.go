92ebf67d2bd3112c9dba0566397e011a72cf8a0f
Andreea Meirosu <ameirosu@uw.co.uk>
2023-07-25 12:16:54 +0200

    correct call on service consumer

--- cmd/eligibility/internal/consumer/service.go
+++ cmd/eligibility/internal/consumer/service.go
@@ -61,7 +61,7 @@ func HandleService(s ServiceStore, evaluator Evaluator, stateRebuild bool) subst
 				}
 
 				if !stateRebuild {
-					occupancyID := inner.(servicer).GetOccupancyId()
+					occupancyID := service.(servicer).GetOccupancyId()
 					err = evaluator.RunFull(ctx, occupancyID)
 					if err != nil {
 						return fmt.Errorf("failed to run evaluation for msg %s, occupancyID %s: %w", env.GetUuid(), occupancyID, err)
