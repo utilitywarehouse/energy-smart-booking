d1214a55f15844ede4aee091d1e51ce0d48f16a1
Andreea Meirosu <ameirosu@uw.co.uk>
2023-07-25 12:56:39 +0200

    do not run on empty occupancy id

--- cmd/eligibility/internal/consumer/service.go
+++ cmd/eligibility/internal/consumer/service.go
@@ -62,9 +62,11 @@ func HandleService(s ServiceStore, evaluator Evaluator, stateRebuild bool) subst
 
 				if !stateRebuild {
 					occupancyID := service.(servicer).GetOccupancyId()
-					err = evaluator.RunFull(ctx, occupancyID)
-					if err != nil {
-						return fmt.Errorf("failed to run evaluation for msg %s, occupancyID %s: %w", env.GetUuid(), occupancyID, err)
+					if occupancyID != "" {
+						err = evaluator.RunFull(ctx, occupancyID)
+						if err != nil {
+							return fmt.Errorf("failed to run evaluation for msg %s, occupancyID %s: %w", env.GetUuid(), occupancyID, err)
+						}
 					}
 				}
 			}
