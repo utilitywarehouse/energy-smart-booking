c32b6230cf28a4adfac3bb9fc00d863259a2b5ba
neiln3121 <30442615+neiln3121@users.noreply.github.com>
2023-07-28 09:52:26 +0100

    Add skeleton API service (#24)
    * Add skeleton API service
    
    * Update actions version

---
+++ cmd/lowribeck-api/internal/api/grpc.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"context"
+
+	contract "github.com/utilitywarehouse/energy-contracts/pkg/generated/third_party/lowribeck/v1"
+	"github.com/utilitywarehouse/energy-smart-booking/cmd/lowribeck-api/internal/lowribeck"
+	"github.com/utilitywarehouse/energy-smart-booking/cmd/lowribeck-api/internal/mapper"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type Client interface {
+	GetCalendarAvailability(context.Context, *lowribeck.GetCalendarAvailabilityRequest) (*lowribeck.GetCalendarAvailabilityResponse, error)
+	CreateBooking(context.Context, *lowribeck.CreateBookingRequest) (*lowribeck.CreateBookingResponse, error)
+}
+
+type LowriBeckAPI struct {
+	client Client
+	contract.UnimplementedLowriBeckAPIServer
+}
+
+func New(c Client) *LowriBeckAPI {
+	return &LowriBeckAPI{
+		client: c,
+	}
+}
+
+func (l *LowriBeckAPI) GetAvailableSlots(ctx context.Context, req *contract.GetAvailableSlotsRequest) (*contract.GetAvailableSlotsResponse, error) {
+	availabilityReq := mapper.MapAvailabilityRequest(req)
+	resp, err := l.client.GetCalendarAvailability(ctx, availabilityReq)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "error making request: %s", err.Error())
+	}
+	return mapper.MapAvailableSlotsResponse(resp)
+}
+
+func (l *LowriBeckAPI) CreateBooking(ctx context.Context, req *contract.CreateBookingRequest) (*contract.CreateBookingResponse, error) {
+	bookingReq := mapper.MapBookingRequest(req)
+
+	resp, err := l.client.CreateBooking(ctx, bookingReq)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "error making request: %s", err.Error())
+	}
+
+	return mapper.MapBookingResponse(resp), nil
+}
