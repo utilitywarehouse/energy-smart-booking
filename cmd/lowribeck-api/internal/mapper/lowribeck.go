fc1bd4a8eefbedfdf666013d0125202e3bc5cc8c
Neil Newbold <nnewbold@uw.co.uk>
2023-07-28 10:13:10 +0100

    Add mappers availability request and responses

--- cmd/lowribeck-api/internal/mapper/lowribeck.go
+++ cmd/lowribeck-api/internal/mapper/lowribeck.go
@@ -1,19 +1,46 @@
 package mapper
 
 import (
+	"fmt"
+	"time"
+
 	contract "github.com/utilitywarehouse/energy-contracts/pkg/generated/third_party/lowribeck/v1"
 	"github.com/utilitywarehouse/energy-smart-booking/cmd/lowribeck-api/internal/lowribeck"
+	"google.golang.org/genproto/googleapis/type/date"
 )
 
-// TODO - SMT-176/get-calendar-availability
+const (
+	sendingSystem         = "UTIL"
+	receivingSystem       = "LB"
+	requestTimeFormat     = "02/01/2006 15:04:05"
+	appointmentDateFormat = "02/01/2006"
+	appointmentTimeFormat = "%d:00-%d:00"
+)
+
 func MapAvailabilityRequest(req *contract.GetAvailableSlotsRequest) *lowribeck.GetCalendarAvailabilityRequest {
-	return &lowribeck.GetCalendarAvailabilityRequest{}
+	return &lowribeck.GetCalendarAvailabilityRequest{
+		PostCode:        req.GetPostcode(),
+		ReferenceID:     req.GetReference(),
+		SendingSystem:   sendingSystem,
+		ReceivingSystem: receivingSystem,
+		CreatedDate:     time.Now().UTC().Format(requestTimeFormat),
+	}
 }
 
-// TODO - SMT-176/get-calendar-availability
 func MapAvailableSlotsResponse(resp *lowribeck.GetCalendarAvailabilityResponse) (*contract.GetAvailableSlotsResponse, error) {
+	slots, err := MapAvailabilitySlots(resp.CalendarAvailabilityResult)
+	if err != nil {
+		return nil, err
+	}
 
-	return &contract.GetAvailableSlotsResponse{}, nil
+	var code *contract.AvailabilityErrorCodes
+	if resp.ResponseCode != "" {
+		*code = MapAvailabilityErrorCodes(resp.ResponseCode, resp.ResponseMessage)
+	}
+	return &contract.GetAvailableSlotsResponse{
+		Slots:      slots,
+		ErrorCodes: code,
+	}, nil
 }
 
 // TODO - SMT-177/create-booking
@@ -24,4 +51,60 @@ func MapBookingRequest(req *contract.CreateBookingRequest) *lowribeck.CreateBook
 // TODO - SMT-177/create-booking
 func MapBookingResponse(_ *lowribeck.CreateBookingResponse) *contract.CreateBookingResponse {
 	return &contract.CreateBookingResponse{}
+}
+
+func MapAvailabilitySlots(availabilityResults []*lowribeck.AvailabilitySlot) ([]*contract.BookingSlot, error) {
+	var err error
+	slots := make([]*contract.BookingSlot, len(availabilityResults))
+	for i, res := range availabilityResults {
+		slots[i] = &contract.BookingSlot{}
+		slots[i].Date, err = MapAppointmentDate(res.AppointmentDate)
+		if err != nil {
+			return nil, fmt.Errorf("error converting appointment date: %v", err)
+		}
+
+		slots[i].StartTime, slots[i].EndTime, err = MapAppointmentTime(res.AppointmentTime)
+		if err != nil {
+			return nil, fmt.Errorf("error converting appointment time: %v", err)
+		}
+	}
+	return slots, nil
+}
+
+func MapAppointmentDate(appointmentDate string) (*date.Date, error) {
+	appDate, err := time.Parse(appointmentDateFormat, appointmentDate)
+	if err != nil {
+		return nil, err
+	}
+
+	return &date.Date{
+		Year:  int32(appDate.Year()),
+		Month: int32(appDate.Month()),
+		Day:   int32(appDate.Day()),
+	}, nil
+
+}
+
+func MapAppointmentTime(appointmentTime string) (int32, int32, error) {
+	var start, end int32
+	read, err := fmt.Sscanf(appointmentTime, appointmentTimeFormat, &start, &end)
+	if err != nil {
+		return -1, -1, err
+	}
+	if read != 2 {
+		return -1, -1, fmt.Errorf("could not find start and end time: %q", appointmentTime)
+	}
+
+	return start, end, nil
+}
+
+// TODO
+func MapAvailabilityErrorCodes(responseCode, responseMessage string) contract.AvailabilityErrorCodes {
+	switch responseMessage {
+	case "EA03":
+		if responseCode != "" {
+			return contract.AvailabilityErrorCodes_AVAILABILITY_INVALID_REQUEST
+		}
+	}
+	return contract.AvailabilityErrorCodes_AVAILABILITY_INTERNAL_ERROR
 }
