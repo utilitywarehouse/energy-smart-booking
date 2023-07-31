7e1228f3b554c3ce6eeaabcc8177a26daaa361b8
Jose Teixeira <jabst1@hotmail.com>
2023-07-31 10:48:58 +0100

    change pool initialisation

--- cmd/booking-api/internal/repository/store/store.go
+++ cmd/booking-api/internal/repository/store/store.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/utilitywarehouse/energy-pkg/postgres"
@@ -12,6 +13,14 @@ func Setup(ctx context.Context, dsn string) (*pgxpool.Pool, error) {
 	pool, err := postgres.Setup(ctx, dsn, migrations.Source)
 	if err != nil {
 		return nil, err
+	}
+	return pool, nil
+}
+
+func GetPool(ctx context.Context, dsn string) (*pgxpool.Pool, error) {
+	pool, err := postgres.Connect(ctx, dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get pool with dsn: %s, %w", dsn, err)
 	}
 	return pool, nil
 }
