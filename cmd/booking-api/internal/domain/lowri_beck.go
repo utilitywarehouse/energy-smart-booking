3225d2a9afe7592279d7f00ebd9a01629d439f84
Neil Newbold <nnewbold@uw.co.uk>
2023-09-08 17:37:35 +0100

    marshal occuapancy span information

--- cmd/booking-api/internal/domain/lowri_beck.go
+++ cmd/booking-api/internal/domain/lowri_beck.go
@@ -2,6 +2,7 @@ package domain
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"time"
@@ -221,9 +222,21 @@ func (d *BookingDomain) findLowriBeckKeys(ctx context.Context, accountID string)
 		}
 		return models.Site{}, models.OccupancyEligibility{}, fmt.Errorf("failed to get live occupancies by accountID, %w", err)
 	}
-	span.AddEvent("response", trace.WithAttributes(attribute.String("site", fmt.Sprintf("%v", site)), attribute.String("occupancyEligible", fmt.Sprintf("%v", occupancyEligible))))
+
+	siteAttr := createSpanAttribute(site, "site", span)
+	occupancyEligibleAttr := createSpanAttribute(occupancyEligible, "occupancyEligible", span)
+	span.AddEvent("response", trace.WithAttributes(siteAttr, occupancyEligibleAttr))
 
 	return *site, *occupancyEligible, nil
+}
+
+func createSpanAttribute(v any, kind string, span trace.Span) attribute.KeyValue {
+	bytes, err := json.Marshal(v)
+	if err != nil {
+		tracing.RecordSpanError(span, err)
+		return attribute.KeyValue{}
+	}
+	return attribute.String(kind, string(bytes))
 }
 
 func mapLowribeckVulnerabilities(vulnerabilities []bookingv1.Vulnerability) (lbVulnerabilities []lowribeckv1.Vulnerability) {
