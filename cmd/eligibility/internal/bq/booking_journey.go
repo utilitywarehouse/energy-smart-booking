35030ebb10580528225e21356975b7aa726d10e2
andreeamei <117647110+andreeamei@users.noreply.github.com>
2023-09-15 14:01:56 +0300

    SMT-252 (#128)
    * add indexer for booking journey eligibility events
    
    * rename to reference

---
+++ cmd/eligibility/internal/bq/booking_journey.go
@@ -0,0 +1,78 @@
+package bq
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"cloud.google.com/go/bigquery"
+	"github.com/sirupsen/logrus"
+	energy_contracts "github.com/utilitywarehouse/energy-contracts/pkg/generated"
+	"github.com/utilitywarehouse/energy-contracts/pkg/generated/smart"
+	"github.com/utilitywarehouse/energy-pkg/metrics"
+	"github.com/uw-labs/substrate"
+	"google.golang.org/protobuf/proto"
+)
+
+type BookingJourneyEligibilityRef struct {
+	OccupancyID string    `bigquery:"occupancy_id"`
+	BookingRef  string    `bigquery:"reference"`
+	Eligible    bool      `bigquery:"eligible"`
+	UpdatedAt   time.Time `bigquery:"updated_at"`
+}
+
+type BookingJourneyEligibilityIndexer struct {
+	client  *bigquery.Client
+	dataset string
+	table   string
+}
+
+func NewBookingJourneyEligibilityIndexer(client *bigquery.Client, dataset, table string) *BookingJourneyEligibilityIndexer {
+	return &BookingJourneyEligibilityIndexer{
+		client:  client,
+		dataset: dataset,
+		table:   table,
+	}
+}
+
+func (i *BookingJourneyEligibilityIndexer) Handle(ctx context.Context, message substrate.Message) error {
+	var env energy_contracts.Envelope
+	if err := proto.Unmarshal(message.Data(), &env); err != nil {
+		return err
+	}
+
+	if env.Message == nil {
+		logrus.Info("skipping empty message")
+		metrics.SkippedMessageCounter.WithLabelValues("empty_message").Inc()
+		return nil
+	}
+
+	inner, err := env.Message.UnmarshalNew()
+	if err != nil {
+		return fmt.Errorf("error unmarshaling event [%s] %s: %w", env.GetUuid(), env.GetMessage().GetTypeUrl(), err)
+	}
+	switch x := inner.(type) {
+	default:
+		return nil
+	case *smart.SmartBookingJourneyOccupancyAddedEvent:
+		update := BookingJourneyEligibilityRef{
+			OccupancyID: x.GetOccupancyId(),
+			BookingRef:  x.GetReference(),
+			Eligible:    true,
+			UpdatedAt:   env.OccurredAt.AsTime(),
+		}
+		err = i.client.Dataset(i.dataset).Table(i.table).Inserter().Put(ctx, update)
+	case *smart.SmartBookingJourneyOccupancyRemovedEvent:
+		update := BookingJourneyEligibilityRef{
+			OccupancyID: x.GetOccupancyId(),
+			Eligible:    false,
+			UpdatedAt:   env.OccurredAt.AsTime(),
+		}
+		err = i.client.Dataset(i.dataset).Table(i.table).Inserter().Put(ctx, update)
+	}
+	if err != nil {
+		return fmt.Errorf("failed to index booking journey eligibility event %s: %w", env.Uuid, err)
+	}
+
+	return nil
+}
