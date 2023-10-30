330d62c29351f5f185559a253be6ef13f0ecc978
Karl Birch <kbirch@uw.co.uk>
2023-10-30 23:34:11 +0000

    feat: optional flake devshell for convenience

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "devshell for energy-smart-booking";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = import nixpkgs { inherit system; };
+    in with pkgs; {
+      devShells.default =
+        mkShell { buildInputs = [ go_1_21 gopls golangci-lint ]; };
+    });
+}
