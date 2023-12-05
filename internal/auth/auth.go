b12f1fda400583deb5973fc977ba2fbd8a5f9ffe
Neil Newbold <nnewbold@uw.co.uk>
2023-12-05 14:12:19 +0000

    Use new resource

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -35,6 +35,7 @@ const (
 	AccountBookingResource = "uw.energy-smart.v1.account.booking"
 	LowribeckAPIResource   = "uw.energy-smart.v1.lowribeck-wrapper"
 	EligibilityResource    = "uw.energy-smart.v1.account.eligibility"
+	POSResource            = "uw.energy-smart.v1.booking.pos-of-sale"
 
 	GetAction    = "get"
 	CreateAction = "create"
