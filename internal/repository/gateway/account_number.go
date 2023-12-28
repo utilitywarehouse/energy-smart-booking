6982d65cd2a5dbd7ff60cafdaacb703e13833593
Jose Teixeira <jabst1@hotmail.com>
2023-12-28 12:12:12 +0000

    adds missing files

---
+++ internal/repository/gateway/account_number.go
@@ -0,0 +1,38 @@
+package gateway
+
+import (
+	"context"
+	"fmt"
+
+	accountService "github.com/utilitywarehouse/account-platform-protobuf-model/gen/go/account/api/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type AccountNumberGateway struct {
+	mai    MachineAuthInjector
+	client AccountNumberClient
+}
+
+func NewAccountNumberGateway(mai MachineAuthInjector, client AccountNumberClient) *AccountNumberGateway {
+	return &AccountNumberGateway{mai, client}
+}
+
+func (gw *AccountNumberGateway) AccountNumber(ctx context.Context, accountID string) (string, error) {
+
+	response, err := gw.client.AccountNumber(gw.mai.ToCtx(ctx), &accountService.AccountNumberRequest{
+		AccountId: []string{accountID},
+	})
+	if err != nil {
+		code := status.Convert(err).Code()
+		accountAPIResponses.WithLabelValues(code.String()).Inc()
+		switch code {
+		case codes.NotFound:
+			return "", fmt.Errorf("%w, %w", ErrAccountNotFound, err)
+		default:
+			return "", fmt.Errorf("failed to get account number for account ID: %s, %w", accountID, err)
+		}
+	}
+
+	return response.AccountNumber[0], nil
+}
---
+++ internal/repository/gateway/account_number_test.go
@@ -0,0 +1,76 @@
+package gateway_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"
+	accountService "github.com/utilitywarehouse/account-platform-protobuf-model/gen/go/account/api/v1"
+	"github.com/utilitywarehouse/energy-smart-booking/internal/repository/gateway"
+	mock_gateways "github.com/utilitywarehouse/energy-smart-booking/internal/repository/gateway/mocks"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func Test_GetAccountNumberByAccountID(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	ctx := context.Background()
+
+	defer ctrl.Finish()
+
+	m := mock_gateways.NewMockAccountNumberClient(ctrl)
+	mai := mock_gateways.NewMockMachineAuthInjector(ctrl)
+
+	mai.EXPECT().ToCtx(ctx).Return(ctx)
+
+	myGw := gateway.NewAccountNumberGateway(mai, m)
+
+	m.EXPECT().AccountNumber(ctx, &accountService.AccountNumberRequest{
+		AccountId: []string{"account-id-1"},
+	}).Return(&accountService.AccountNumberResponse{
+		AccountNumber: []string{"80001"},
+	}, nil)
+
+	actual := "80001"
+
+	expected, err := myGw.AccountNumber(ctx, "account-id-1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if diff := cmp.Diff(expected, actual); diff != "" {
+		t.Fatal(diff)
+	}
+}
+
+func Test_GetAccountNumberByAccountID_NotFound(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	ctx := context.Background()
+
+	defer ctrl.Finish()
+
+	m := mock_gateways.NewMockAccountNumberClient(ctrl)
+	mai := mock_gateways.NewMockMachineAuthInjector(ctrl)
+
+	mai.EXPECT().ToCtx(ctx).Return(ctx)
+
+	myGw := gateway.NewAccountNumberGateway(mai, m)
+
+	m.EXPECT().AccountNumber(ctx, &accountService.AccountNumberRequest{
+		AccountId: []string{"account-id-1"},
+	}).Return(&accountService.AccountNumberResponse{
+		AccountNumber: []string{},
+	}, status.Error(codes.NotFound, "not found"))
+
+	expectedErr := fmt.Errorf("%w, %w", gateway.ErrAccountNotFound, status.Error(codes.NotFound, "not found"))
+
+	_, actualErr := myGw.AccountNumber(ctx, "account-id-1")
+
+	if diff := cmp.Diff(actualErr.Error(), expectedErr.Error()); diff != "" {
+		t.Fatal(diff)
+	}
+}
