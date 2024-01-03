4f19f34dd1cb3c4a9429479863c4d27b75484780
Neil Newbold <nnewbold@uw.co.uk>
2024-01-03 10:40:17 +0000

    Remove get Pool

--- cmd/booking-api/internal/repository/store/store.go
+++ cmd/booking-api/internal/repository/store/store.go
@@ -2,7 +2,6 @@ package store
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/utilitywarehouse/energy-pkg/postgres"
@@ -13,14 +12,6 @@ func Setup(ctx context.Context, dsn string) (*pgxpool.Pool, error) {
 	pool, err := postgres.Setup(ctx, dsn, migrations.Source)
 	if err != nil {
 		return nil, err
-	}
-	return pool, nil
-}
-
-func GetPool(ctx context.Context, dsn string) (*pgxpool.Pool, error) {
-	pool, err := postgres.Connect(ctx, dsn)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get pool, %w", err)
 	}
 	return pool, nil
 }
