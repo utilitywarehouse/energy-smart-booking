53b2407e071419f5c2b39a200add35d5e7d5e4dd
Oskar Domingos <odomingos@uw.co.uk>
2024-02-26 16:05:25 +0000

    new column added to partial_booking

--- internal/models/partial_booking.go
+++ internal/models/partial_booking.go
@@ -6,11 +6,21 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
+type DeletionReason int32
+
+const (
+	// BookingCreated marks that the booking was marked as deleted due to the creation of full booking
+	BookingCreated DeletionReason = iota
+	// BookingExpired marks that the booking was marked as deleted due to the lack of occupancy for more than three weeks
+	BookingExpired
+)
+
 type PartialBooking struct {
-	BookingID string
-	Event     proto.Message
-	CreatedAt time.Time
-	UpdatedAt *time.Time
-	DeletedAt *time.Time
-	Retries   int
+	BookingID      string
+	Event          proto.Message
+	CreatedAt      time.Time
+	UpdatedAt      *time.Time
+	DeletedAt      *time.Time
+	Retries        int
+	DeletionReason *DeletionReason
 }
