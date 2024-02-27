67d3a52a79745b28e580e3fa80599691724da1f5
Oskar Domingos <odomingos@uw.co.uk>
2024-02-27 11:38:24 +0000

    test case for booking expired added

--- cmd/booking-api/internal/workers/partial_booking_test.go
+++ cmd/booking-api/internal/workers/partial_booking_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/utilitywarehouse/energy-smart-booking/internal/models"
 )
 
-func Test_Run(t *testing.T) {
+func Test_CompletedPartialBooking(t *testing.T) {
 	ctrl := gomock.NewController(t)
 
 	ctx := context.Background()
@@ -71,6 +71,60 @@ func Test_Run(t *testing.T) {
 	}, gomock.Any()).Return(nil)
 
 	mockPBStore.EXPECT().MarkAsDeleted(ctx, "booking-id-1", models.DeletionReasonBookingCreated).Return(nil)
+
+	mockPBStore.EXPECT().UpdateRetries(ctx, "booking-id-2", 0).Return(nil)
+
+	err := worker.Run(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test_ExpiredPartialBookings(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	ctx := context.Background()
+
+	defer ctrl.Finish()
+
+	mockPBStore := mocks.NewMockPartialBookingStore(ctrl)
+	mockOccupancyStore := mocks.NewMockOccupancyStore(ctrl)
+	mockPublisher := mocks.NewMockBookingPublisher(ctrl)
+	alertThreshold := time.Hour
+
+	worker := workers.NewPartialBookingWorker(mockPBStore, mockOccupancyStore, mockPublisher, alertThreshold)
+
+	mockPBStore.EXPECT().GetPending(ctx).Return([]*models.PartialBooking{
+		{
+			CreatedAt: time.Now().AddDate(0, 0, -23),
+			BookingID: "booking-id-1",
+			Event: &bookingv1.BookingCreatedEvent{
+				BookingId:   "booking-id-1",
+				OccupancyId: "",
+				Details: &bookingv1.Booking{
+					Id:        "booking-id-1",
+					AccountId: "account-id-1",
+				},
+			},
+		},
+		{
+			CreatedAt: time.Now(),
+			BookingID: "booking-id-2",
+			Event: &bookingv1.BookingCreatedEvent{
+				BookingId:   "booking-id-2",
+				OccupancyId: "",
+				Details: &bookingv1.Booking{
+					Id:        "booking-id-2",
+					AccountId: "account-id-2",
+				},
+			},
+		},
+	}, nil)
+
+	mockOccupancyStore.EXPECT().GetOccupancyByAccountID(ctx, "account-id-1").Return(nil, store.ErrOccupancyNotFound)
+	mockOccupancyStore.EXPECT().GetOccupancyByAccountID(ctx, "account-id-2").Return(nil, store.ErrOccupancyNotFound)
+
+	mockPBStore.EXPECT().MarkAsDeleted(ctx, "booking-id-1", models.DeletionReasonBookingExpired).Return(nil)
 
 	mockPBStore.EXPECT().UpdateRetries(ctx, "booking-id-2", 0).Return(nil)
 
